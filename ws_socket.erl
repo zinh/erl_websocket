d8eb440e8361083c8b6815bc3fca6d8f9af6cc51
Vinh Nguyen <xuanvinhct@gmail.com>
2015-06-13 19:58:53 +0700

    add initial code

---
+++ ws_socket.erl
@@ -0,0 +1,102 @@
+-module(ws_socket).
+-compile(export_all).
+-define(MAGIC_STRING, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").
+
+start_link(Port) ->
+  Pid = spawn(fun() -> 
+        {ok, LSock} = gen_tcp:listen(Port, [binary, {active, false}]),
+        spawn(fun() -> acceptor(LSock) end),
+        timer:sleep(infinity)
+    end),
+  {ok, Pid}.
+
+
+acceptor(LSock) ->
+  {ok, Socket} = gen_tcp:accept(LSock),
+  spawn(fun() -> acceptor(LSock) end),
+  handle(Socket).
+
+handle(Socket) ->
+  handshake(Socket),
+  recv(Socket).
+
+handshake(Socket) ->
+  inet:setopts(Socket, [{active, once}]),
+  receive
+    {tcp, Socket, Packet} ->
+      Headers = get_path(Packet),
+      Key = proplists:get_value("Sec-Websocket-Key", Headers),
+      AcceptKey = websocket_key(Key),
+      Data = [<<"HTTP/1.1 101 Switching Protocols\r\n",
+        "Upgrade: websocket\r\n",
+        "Connection: Upgrade\r\n",
+        "Sec-WebSocket-Accept: ">>,
+        AcceptKey,
+        <<"\r\n\r\n">>],
+      io:format("~s~n", [Data]),
+      gen_tcp:send(Socket, Data)
+  end.
+
+recv(Socket) ->
+  inet:setopts(Socket, [{active, once}]),
+  receive
+    {tcp, Socket, Frame} ->
+      case Frame of
+        <<Fin:1, _:3, Opcode:4, Mask:1, PayloadLen:7, Key:32, Payload/binary>> when PayloadLen < 126 ->
+          Message = decode(Key, Payload),
+          io:format("~p~n", [Message]);
+        _ ->
+          io:format("Bad format~n")
+      end
+  end.
+
+decode(Key, Data) when is_binary(Key) ->
+  io:format("Key: ~s~n", [Key]),
+  Keys = bin_to_list(Key, 8),
+  decode(Keys, Data);
+
+decode(Keys, Data) when is_list(Keys) ->
+  decode(Keys, Data, []).
+
+decode([H | T], Data, DecodedData) ->
+  case Data of
+    <<D:8, Rest/binary>> -> 
+      DecodedData ++ [D bxor H],
+      decode(T ++ H, Rest, DecodedData);
+    _ ->
+      DecodedData
+  end.
+
+
+bin_to_list(Data, Bitlen) ->
+  bin_to_list(Data, Bitlen, []).
+
+bin_to_list(Data, Bitlen, List) ->
+  case Data of
+    <<T:Bitlen/binary, Rest/binary>> ->
+      bin_to_list(Rest, Bitlen, List ++ T);
+    _ ->
+      List
+  end.
+
+% private
+get_path(Packet) ->
+  {ok, {http_request, _, {abs_path, Path}, _}, Rest} = erlang:decode_packet(http, Packet, []),
+  parse_header(Rest).
+
+parse_header(Packet) ->
+  parse_header(Packet, []).
+
+parse_header(Packet, Headers) ->
+  case erlang:decode_packet(httph, Packet, []) of 
+    {ok, {http_header, _, Key, _, Value}, Rest} ->
+      parse_header(Rest, [{Key, Value} | Headers]);
+    {ok, http_eoh, _} ->
+      Headers;
+    {error, _Reason} ->
+      Headers
+  end.
+
+websocket_key(Key) ->
+  HashKey = crypto:hash(sha, Key ++ ?MAGIC_STRING),
+  base64:encode(HashKey).
